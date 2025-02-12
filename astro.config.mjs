346cb1e1e63725359249ce65dadaf756d3c2171b
zooeywm <zooeywm@gmail.com>
2025-02-12 11:03:47 +0800

    fix: giscus

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bartoszlenar.github.io",
+  site: "https://zooeywm.github.io",
   base: "/astro-blog/",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
